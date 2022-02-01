00d8f2a5588af5934ae2caf828620d35fe0c6a6c
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-02-01 10:40:33 -0700

    Add nameCapitalized function

--- exercises/javaScriptExercises/programmingBasics/printName.js
+++ exercises/javaScriptExercises/programmingBasics/printName.js
@@ -1,5 +1,5 @@
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const myName = letters[19] + letters[0] + letters[12] + letters[0] + letters[17] + letters[0];
+const nameCapitalized = myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase();
 
-
-console.log(myName); // => TAMARA
+console.log(nameCapitalized); // => TAMARA
