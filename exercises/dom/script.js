1bae9affd63b0f4da0b0c3397c1d06aed2370b9f
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-02-04 14:44:48 -0700

    Add dom exercise

--- exercises/dom/script.js
+++ exercises/dom/script.js
@@ -1,21 +1,25 @@
 const navbar = document.getElementById('nav-bar');
-console.log(navbar);
 const main = document.querySelector('main');
-console.log(main);
-const tag1 = document.querySelectorAll('a');
-console.log(tag1)
 const tag2 = document.querySelector('a');
-console.log(tag2)
 const logo = document.querySelector('.logo');
+const getAnchorTag = document.querySelectorAll('a');
 
 
 function timer() {
     logo.innerText = 'Hi';
 }
-
 setTimeout(timer, 1000);
 
 setTimeout(() => { console.log(logo.innerHTML = 'Hi') }, 1000);
 
 const nameChange = document.getElementsByClassName('header-item--right')[0];
 const changedName = nameChange.innerText = 'Tamara Carlos';
+
+
+function timer2() {
+    for (i = 0; i < getAnchorTag.length; i++) {
+        getAnchorTag[i].style.textDecoration = 'underline';
+    }
+}
+// timer2();
+setTimeout(timer2, 2000);
